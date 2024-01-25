55dba5b84a44e436f52f45650c2684b13036eeed
Juliusz Pawlus <juliuszpawlus@gmail.com>
2024-01-25 15:55:19 +0100

    Added visualization for speeding busses and saving speeding distances from analytics to csv file.

--- Analytics.py
+++ Analytics.py
@@ -3,6 +3,7 @@ import math
 import os
 import pandas as pd
 import numpy as np
+import csv
 
 
 # Creating new data frame made from a file named 'tag' in 'storage' directory.
@@ -34,12 +35,15 @@ def haversine_velocity(row):
         b = (np.cos(row['Lat'] * math.pi / 180)
              * np.cos(row['Lat2'] * math.pi / 180)
              * pow((np.sin(row['DelLon'] * math.pi / 360)), 2))
-        haversine = 2 * r * np.arcsin(np.sqrt(a + b))
-        return haversine / row['DelTime'] * 3.6
+        haversine = 2 * r * np.arcsin(np.sqrt(a + b)) / row['DelTime'] * 3.6
+        if haversine > 160:  # Detecting impossible data.
+            return np.NAN
+        else:
+            return haversine
 
 
 # Finds buses, which exceeded the speed of 50 km/h between two states in time.
-def check_velocity(frame1, frame2):
+def check_velocity(frame1, frame2, to_file=False, file_name='BUS_SPEEDING'):
     velocity = 50
     ans = np.subtract(frame2, frame1)  # Calculating change in location and time between two states.
     frame1.drop('Time', axis=1, inplace=True)  # Dropping no longer needed columns.
@@ -51,18 +55,34 @@ def check_velocity(frame1, frame2):
     ans['DelTime'] = ans['DelTime'].dt.total_seconds()
     ans['Velocity'] = ans.apply(haversine_velocity, axis=1)  # Calculating average velocity.
     ans = ans.loc[ans['Velocity'] > velocity]  # Choosing only buses which passed 50 km/h
+    if to_file:
+        path = os.path.join(os.getcwd(), file_name + ".csv")
+        distances = ans[['Lon', 'Lat', 'Lon2', 'Lat2']].values.tolist()
+        with open(path, 'a') as file:
+            writer = csv.writer(file)
+            for dist in distances:
+                writer.writerow(dist)
+        file.close()
+
     return ans.index.tolist()
 
 
-# Counts buses that exceeded the speed of 50 km/h in given period of time.
-def count_passed(tag='', storage='DATA', length=60):
+# Counts different bus numbers that exceeded the speed of 50 km/h in given period of time.
+# Saves data in csv file if needed.
+def count_passed(tag='', storage='DATA', length=60, to_file=False, file_name='BUS_SPEEDING'):
     busses = set()  # Set of busses that exceeded the speed.
     if length > 1:
+        if to_file:  # Creating an empty file.
+            path = os.path.join(os.getcwd(), file_name + ".csv")
+            with open(path, "w") as file:
+                writer = csv.writer(file)
+                writer.writerow(['Lon', 'Lat', 'Lon2', 'Lat2'])
+                file.close()
         group = 0
         frame1 = create_frame(tag + '1', storage)
         for i in range(2, length + 1):
             frame2 = create_frame(tag + str(i), storage)
-            too_fast = check_velocity(frame1.copy(), frame2.copy())
+            too_fast = check_velocity(frame1.copy(), frame2.copy(), to_file, file_name)
             frame1 = frame2
             group = group + len(too_fast)
             for el in too_fast:  # Adding new buses that exceeded the speed.
@@ -72,4 +92,4 @@ def count_passed(tag='', storage='DATA', length=60):
         return 0
 
 
-print(count_passed(tag='morning_data_', storage='TUESDAY_MORNING', length=120))
+print(count_passed(tag='morning_', storage='MORNING_DATA', length=100, to_file=True))
---
+++ speeding_map.py
@@ -0,0 +1,38 @@
+import os.path
+
+import plotly.graph_objects as go
+import csv
+
+
+# Shows where buses were speeding on the interactive map.
+def show_speed_map(name_file='BUS_SPEEDING'):
+    path = os.path.join(os.getcwd(), name_file + '.csv')
+    lon = []  # Longitudes of distances.
+    lat = []  # Latitudes of distances.
+
+    with open(path, "r") as file:  # Reading speeding distances.
+        reader = csv.DictReader(file)
+        for row in reader:
+            lon.append([row['Lon'], row['Lon2']])
+            lat.append([row['Lat'], row['Lat2']])
+
+    fig = go.Figure(go.Scattermapbox())  # Map creating.
+
+    for i in range(len(lon)):  # Adding distances to the map.
+        fig.add_trace(go.Scattermapbox(
+            mode="markers+lines",
+            lon=lon[i],
+            lat=lat[i],
+            marker={'size': 10}))
+
+    fig.update_layout(  # Updating map layout.
+        margin={'l': 0, 't': 0, 'b': 0, 'r': 0},
+        mapbox={
+            'center': {'lon': 21.017532, 'lat': 52.237049},
+            'style': "open-street-map",
+            'zoom': 9
+        })
+    fig.show()
+
+
+show_speed_map()
